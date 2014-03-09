ddfde3d67b04ebd1294dfd932bd450976e739ab7
Davide Franco <bacula-dev@dflc.ch>
2014-03-09 20:44:44 +0100

    New application configuration file

---
+++ application/config/bwapp.config.php
@@ -0,0 +1,9 @@
+<?php
+// Don't touch this config file please
+$app_config = array(    'name'          => 'Bacula-Web',
+                        'user_config'   => CONFIG_FILE,
+                        'version'       => '6.0.1',
+                        'author'        => 'Davide Franco',
+                        'website'       => 'http://www.bacula-Web.org',
+                        'debug'         => false);
+?>
