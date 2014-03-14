fd10dd7344ece7acc9c6b3505784ae2955554252
Davide Franco <bacula-dev@dflc.ch>
2014-03-14 15:54:23 +0100

    Moved CTranslation code into Application->bootstrap() function
     - replaced private by protected init() function
     - some code cleanup

--- core/app/view.class.php
+++ core/app/view.class.php
@@ -17,24 +17,39 @@
  */
 
 class View extends Smarty {
+    private $language;
+    private $charset;
+    private $domaine;   
 
     public function __construct() {
-        $this->init();
     }
 
-    private function init() {
+    protected function init() {
+        global $bwapp;
+
         $this->compile_check = true;
         $this->debugging = false;
         $this->force_compile = true;
 
         $this->template_dir = TEMPLATES_DIR;
         $this->compile_dir  = VIEW_CACHE_DIR;
+
+        // Setting up language translation
+        $this->register_block('t', 'smarty_translate');
+        $this->language = FileConfig::get_Value( 'language');
+        $this->charset  = 'UTF-8';
+
+        putenv("LANGUAGE=" . $this->language . '.' . $this->charset);
+        putenv("LANG=" . $this->language . '.' . $this->charset);
+        setlocale(LC_ALL, $this->language . '.' . $this->charset);
+
+        bindtextdomain('messages', LOCALE_DIR);
+        bind_textdomain_codeset('messages', $this->charset);
+        textdomain($this->domaine);
     }
 
     public function render($view = 'index.tpl') {
         $this->display($view);
     }
-
 }
-
 ?>
