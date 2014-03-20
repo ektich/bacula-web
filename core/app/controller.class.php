e7016c0f05a4d249737b7abc6b932731be89d87b
Davide Franco <bacula-dev@dflc.ch>
2014-03-20 10:00:55 +0100

    Removed code handled by RouterController class and some class properties cleanup

--- core/app/controller.class.php
+++ core/app/controller.class.php
@@ -1,18 +1,9 @@
 <?php
 
 class Controller {
-    protected $default_view;
+    protected $view;
 
     public function __construct() {
-        $this->default_view = 'DashBoardView';
-    }
-  
-    public function getView() {
-       if( !is_null( ChttpRequest::get_Value('page') ) ) {
-           return ChttpRequest::get_Value('page');
-       }else{
-           return $this->default_view;
-       }
     }
 }
 ?>
