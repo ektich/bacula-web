0191087584d61997acd86c1c6c77723e56f4bdaa
Davide Franco <bacula-dev@dflc.ch>
2014-03-21 10:17:07 +0100

    Removed all database related code from Model base class

--- core/app/model.class.php
+++ core/app/model.class.php
@@ -15,29 +15,9 @@
   +-------------------------------------------------------------------------+
  */
 
-class Model {
-   protected $dbadapter;
-
-   public function __construct() 
+class Model
+{
+   public function __construct()
    {
-       $this->dbadapter = new DatabaseAdapter();
    }
- 
-   // ==================================================================================
-   // Function: 	count()
-   // Parameters:	$tablename
-   //				$filter (optional)
-   // Return:		return row count for one table
-   // ==================================================================================
-	
-	protected function count( $tablename, $filter = null ) {
-		$fields		= array( 'COUNT(*) as row_count' );
-
-		// Prepare and execute query
-		$statment 	= CDBQuery::get_Select( array( 'table' => $tablename, 'fields' => $fields, $filter) );
-		$result 	= CDBUtils::runQuery($statment, $this->db_link);
-
-		$result 	= $result->fetch();
-		return $result['row_count'];		
-	}
- }
+}
