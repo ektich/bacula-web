83e2a608f346178d6a331c905bb27542431ea9d4
Davide Franco <bacula-dev@dflc.ch>
2014-03-21 10:14:37 +0100

    New DatabaseModel class file
     - this class purpose is to provide a specific model which need PDO database connection.
       For example, the Exception Model class doesn't need a database connection. That's the reason to make a difference between Model base class and DatabaseModel

---
+++ core/app/databasemodel.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class DatabaseModel extends Model
+{
+   private $dbadapter;
+
+   public function __construct()
+   {
+     $this->dbadapter = new DatabaseAdapter();
+   }
+
+   // ==================================================================================
+   // Function:         count()
+   // Parameters:       $tablename
+   //                           $filter (optional)
+   // Return:           return row count for one table
+   // ==================================================================================
+
+   protected function count($tablename, $filter = null)
+   {
+        $fields         = array( 'COUNT(*) as row_count' );
+
+        // Prepare and execute query
+        $statment       = CDBQuery::get_Select( array( 'table' => $tablename, 'fields' => $fields, $filter) );
+        $result         = CDBUtils::runQuery($statment, $this->db_link);
+
+        $result         = $result->fetch();
+
+        return $result['row_count'];
+   }
+}
