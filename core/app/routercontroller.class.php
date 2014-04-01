0314ca0331b53b06394225812f020edf25ddb1fd
Davide Franco <bacula-dev@dflc.ch>
2014-04-01 09:44:31 +0200

    Several improvments in RouterController class
     - new getController() function
     - new getModel() function
     - new getView() function
     - new getContext() function
     - added default context parameter in constructor() function

--- core/app/routercontroller.class.php
+++ core/app/routercontroller.class.php
@@ -18,16 +18,65 @@
 
 class RouterController {
     private $context = null;
+    private $default_context;
 
-    public function __construct() {
+    public function __construct($default_context) {
+        $this->default_context = $default_context;
+
         if (isset( $_GET['page']))
             $this->context = $_GET['page'];
+        else
+            $this->context = $this->default_context;
     }
 
     public function getContext() {
-         return $this->context;
+         if(!is_null($this->context))
+             return $this->context;
+         else
+             return false;
+    }
+
+    public function getController() {   
+        $classname = '';
+
+        if(!is_null($this->getContext())) {
+            $classname = ucfirst($this->context) . '_Controller';
+
+            if( !class_exists($classname) )
+                throw new Exception( "Controller class ($classname) do not exist" );
+        }else
+            $classname = 'Dashboard_Controller';
+ 
+        return $classname;
+    }
+
+    public function getView() {
+        $classname = '';
+
+        if(!is_null($this->getContext())) {
+            $classname = ucfirst($this->context) . '_View';
+
+            if( !class_exists($classname) )
+                throw new Exception( "View class ($classname) do not exist" );
+        }else
+            $classname = 'Dashboard_View';
+
+        return $classname;
+    }
+
+    public function getModel() {
+        $classname = '';
+
+        if(!is_null($this->getContext())) {
+            $classname = ucfirst($this->context) . '_Model';
+
+            if( !class_exists($classname) )
+                throw new Exception( "Model class ($classname) do not exist" );
+        }else
+            $classname = 'Dashboard_Model';
+
+        return $classname;
     }
 }
-
 ?>
 
